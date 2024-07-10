c5b9767cd3dc40584a13e6534a6c21fe81c3e46b
Keshaunj <keshaunjones48@gmail.com>
2024-07-10 05:16:14 -0400

    update fromjuly 10 morning

---
+++ script.js
@@ -0,0 +1,12 @@
+/*
+Exercise 3: Insert at the beginning
+
+1) Insert the string 'taco' at the beginning of the `foods` array.
+
+Complete Exercise 3 in the space below:
+*/
+
+foods.unshift
+
+
+console.log('Exercise 3 result:', foods);
