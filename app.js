1bf0422bba7aec31154c3c4dc7c7b51f2fab5faf
Keshaunj <keshaunjones48@gmail.com>
2024-07-13 20:37:32 -0400

    done

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ Exercise 1: Define an empty array
 Exercise 1 has been completed for you:
 */
 
-const foods = [];  
+const foods = [];
 
-console.log('Exercise 1 result:', foods);
+console.log("Exercise 1 result:", foods);
 
 /*
 Exercise 2: Add strings to the array
@@ -20,9 +20,9 @@ Note: 'pizza' should be the first item in the array, followed by 'cheeseburger'.
 Complete Exercise 2 in the space below:
 */
 
-foods.push('pizza', 'cheeseburger')
+foods.push("pizza", "cheeseburger");
 
-console.log('Exercise 2 result:', foods);
+console.log("Exercise 2 result:", foods);
 
 /*
 Exercise 3: Insert at the beginning
@@ -32,14 +32,9 @@ Exercise 3: Insert at the beginning
 Complete Exercise 3 in the space below:
 */
 
+foods.unshift("taco");
 
-
-foods.unshift('taco')
-
-
-
-console.log('Exercise 3 result:', foods); 
-
+console.log("Exercise 3 result:", foods);
 
 /*
 Exercise 4: Access an array element
@@ -52,11 +47,11 @@ Exercise 4: Access an array element
 Complete Exercise 4 in the space below:
 */
 
-let pizza = foods[1]
+let pizza = foods[1];
 
-let favFood = pizza
+let favFood = pizza;
 
-console.log('Exercise 4 result:', favFood);
+console.log("Exercise 4 result:", favFood);
 //executes the output of: pizza
 /*
 Exercise 5: Insert an element between two others
@@ -65,13 +60,12 @@ Exercise 5: Insert an element between two others
 
 Complete Exercise 5 in the space below:
 */
-//first argument is my postion selector and 
+//first argument is my postion selector and
 //2nd agrument is my number of items to dlete from "wwww.javascripttutorial.net"
 
-foods.splice(2,0,'tofu');
+foods.splice(2, 0, "tofu");
 
-
-console.log('Exercise 5 result:', foods);
+console.log("Exercise 5 result:", foods);
 
 //prints // Exercise 5 result: [ 'taco', 'pizza', 'tofu', 'cheeseburger' ]
 
@@ -82,14 +76,10 @@ Exercise 6: Replace elements
 
 Complete Exercise 6 in the space below:
 */
- //assiuming that to replace pizza means to delete it and input 'sushi' and 'cupcake' in array [1].
-foods.splice(1,1,'sushi','cupcake')
+//assiuming that to replace pizza means to delete it and input 'sushi' and 'cupcake' in array [1].
+foods.splice(1, 1, "sushi", "cupcake");
 
-
-console.log('Exercise 6 result:', foods); // runs Exercise 6 result: [ 'taco', 'sushi', 'cupcake', 'tofu', 'cheeseburger' ]
-
-
-
+console.log("Exercise 6 result:", foods); // runs Exercise 6 result: [ 'taco', 'sushi', 'cupcake', 'tofu', 'cheeseburger' ]
 
 /*
 Exercise 7: Using the `slice()` method
@@ -102,12 +92,12 @@ Exercise 7: Using the `slice()` method
 Complete Exercise 7 in the space below:
 */
 
-let yummy = ['sushi','cupcake']
+let yummy = ["sushi", "cupcake"];
 
-foods.slice(0)
-foods.slice(3,2)
+foods.slice(0);
+foods.slice(3, 2);
 
-console.log('Exercise 7 result:', yummy);
+console.log("Exercise 7 result:", yummy);
 // prints Exercise 7 result: [ 'sushi', 'cupcake' ]
 
 /*
@@ -120,19 +110,12 @@ Exercise 8: Finding an index
 
 Complete Exercise 8 in the space below:
 */
- 
 
-let soyIdx = foods.indexOf('tofu');
+let soyIdx = foods.indexOf("tofu");
 
 //console.log(foods.indexOf('tofu'))
 
-console.log('Exercise 8 result:', soyIdx); // i think i did something wrong but previous code has  spliced out/deleted 'tofu' out of the array or not.The indexOf() method searches for a specified element within an array and returns the index of the first occurrence. If the element is not found, it returns -1. Here are the docs ----< second sentece i dragged and drop from the lab examples for my refrence.
-
-
-
-
-
-
+console.log("Exercise 8 result:", soyIdx); // i think i did something wrong but previous code has  spliced out/deleted 'tofu' out of the array or not.The indexOf() method searches for a specified element within an array and returns the index of the first occurrence. If the element is not found, it returns -1. Here are the docs ----< second sentece i dragged and drop from the lab examples for my refrence.
 
 /*
 Exercise 9: Joining elements
@@ -148,11 +131,9 @@ Note: The final result should log as:
 Complete Exercise 9 in the space below:
 */
 
+const allFoods = foods.join("->");
 
-const allFoods = foods.join('->')
-
-
-console.log('Exercise 9 result:', allFoods);
+console.log("Exercise 9 result:", allFoods);
 
 /*
 Exercise 10: Check for an element
@@ -165,12 +146,9 @@ Exercise 10: Check for an element
 Complete Exercise 10 in the space below:
 */
 
+let hasSoup = foods.includes("soup");
 
-
-let hasSoup = foods.includes('soup')
-
-console.log('Exercise 10 result:', hasSoup);
-
+console.log("Exercise 10 result:", hasSoup);
 
 /*
 Exercise 11: Odd numbers from an array
@@ -185,18 +163,13 @@ Complete Exercise 11 in the space below:
 */
 
 const nums = [100, 5, 23, 15, 21, 72, 9, 45, 66, 7, 81, 90];
-let odds = []
+let odds = [];
 
 for (let i = 0; i < nums.length; i++) {
-    if (nums[i] % 2 !==0)
-        odds.push(nums[i])
-    
-  }
- 
+  if (nums[i] % 2 !== 0) odds.push(nums[i]);
+}
 
-
-console.log('Exercise 11 result:', odds);
-
+console.log("Exercise 11 result:", odds);
 
 /*
 Exercise 12: FizzBuzz with arrays
@@ -216,13 +189,84 @@ Exercise 12: FizzBuzz with arrays
 
 Complete Exercise 12 in the space below:
 */
+let fizz = [];
+let buzz = [];
+let fizzbuzz = [];
+
 for (let i = 0; i < nums.length; i++) {
-    console.log(nums[i]);
-  }
-  
+  if (nums[i] % 3 === 0 && nums[i] % 5 === 0) fizz.push(nums[i]);
+  if (nums[i] )
+  buzz.push(nums[i]);
+  fizzbuzz.push(nums[i]);
+}
+
+console.log("Exercise 12 Results:");
+console.log("  fizz:", fizz);
+console.log("  buzz:", buzz);
+console.log("  fizzbuzz:", fizzbuzz);
 
 
-console.log('Exercise 12 Results:');
-console.log('  fizz:', fizz);
-console.log('  buzz:', buzz);
-console.log('  fizzbuzz:', fizzbuzz);
+/*
+Exercise 13: Retrieve the Last Array
+
+1) Assign the last nested array in the `numArrays` below to a variable named
+   `numList`. As you do this, also fulfill these goals:
+
+   - Assume you don't know how many nested arrays `numArrays` contains.
+   - Do not alter the original `numArrays` array.
+
+Complete Exercise 13 in the space below:
+*/
+
+
+const numArrays = [
+	[100, 5, 23],
+	[15, 21, 72, 9],
+	[45, 66],
+	[7, 81, 90]
+];
+let numList = numArrays[numArrays.length -1];
+
+console.log('Exercise 13 result:', numList);
+
+/*
+Exercise 14: Accessing within nested arrays
+
+1) Retrieve the number `66` from the `numArrays` array. As part of this process
+   do not alter the original `numArrays` array.
+
+2) Assign it to a variable called `num`.
+
+Complete Exercise 14 in the space below:
+*/
+
+let num = numArrays[2][1]   // first agrument is 0 ,1,2nd nested arrays place with second agrue,index 2 being 66.
+
+console.log('Exercise 14 result:', num);
+
+/*
+Exercise 15: Nested array sum
+
+1) Use nested loops or `forEach()` methods to sum up all numbers within 
+   `numArrays` nested arrays.
+   
+2) Assign the sum to a variable called `total`.
+
+Hint: Be sure to declare and initialize the total variable before the iterations.
+
+Complete Exercise 15 in the space below:
+*/
+
+
+
+ 
+let total = 0;
+
+for (let i = 0; i < numArrays.length; i++) {
+    for (let h = 0; h < numArrays[i].length; h++) {
+        total += numArrays[i][h];
+    }
+}
+
+console.log('Exercise 15 result:\n', total);
+
